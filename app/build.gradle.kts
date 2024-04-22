bc6ff3d6432b259c62a521858487ea029dc92a49
jennifer-s19 <siaojm@yahoo.com>
2024-04-22 15:43:33 -0700

    adding another dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
 //    implementation("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation("net.openid:appauth:0.11.1")
 
-    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("a ndroidx.credentials:credentials:1.2.2")
     implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
 
@@ -61,6 +61,7 @@ dependencies {
     implementation("com.google.oauth-client:google-oauth-client-jetty:1.30.4")
    // implementation("com.google.apis:google-api-services-sheets:v4-rev20220927-2.0.0")
     implementation("com.google.android.gms:play-services-auth:19.2.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
 
     implementation ("com.loopj.android:android-async-http:1.4.11") //for ASync
     implementation ("com.google.android.material:material:1.5.0")
