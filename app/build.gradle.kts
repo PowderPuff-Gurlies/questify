e691dca74e59d3c3e24598257c9626a1dc9f284c
Zooomulus <andelisun@gmail.com>
2024-04-22 18:01:15 -0400

    additional dependcies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 dependencies {
 //    implementation ("com.github.bumptech.glide:glide:4.11.0")
 //    annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
+    implementation("androidx.credentials:credentials:1.3.0-alpha03")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0-alpha03")
     implementation ("com.google.api-client:google-api-client:2.0.0")
     implementation ("com.google.oauth-client:google-oauth-client-jetty:1.34.1")
     implementation ("com.google.apis:google-api-services-calendar:v3-rev20220715-2.0.0")
